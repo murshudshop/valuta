a1e92cbb02acbb28f94b08f23c3070ec82fcda1e
murshud <murshud@gmail.com>
2023-07-08 12:35:25 +0400

    salam

---
+++ test.js
@@ -0,0 +1,10 @@
+let aze = prompt('Deyişmək isdədiyiniz məbləgi qeyd edin');
+let usd = aze / 1.7;
+let eur = aze / 1.85
+let tl = aze / 0.06523
+let rus = aze / 0.01861
+document.getElementById('murik').innerHTML = aze + ' ₼ = ' + usd.toFixed() + 'azn' ;
+document.getElementById('ss').innerHTML = aze + ' ₼ = ' + eur.toFixed() + 'euro' ;
+document.getElementById('cc').innerHTML = aze + ' ₼ = ' + tl.toFixed() + 'tl' ;
+document.getElementById('aa').innerHTML = aze + ' ₼ = ' + rus.toFixed() + 'rubl' ;
+    
