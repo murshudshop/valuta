a521ac9cb5aac5dca60dd37831c173847f835b63
murshud <murshud@gmail.com>
2023-07-08 12:56:14 +0400

    sa

--- test.js
+++ test.js
@@ -3,7 +3,7 @@ let usd = aze / 1.7;
 let eur = aze / 1.85
 let tl = aze / 0.06523
 let rus = aze / 0.01861
-document.getElementById('murik').innerHTML = aze + ' ₼ = ' + usd.toFixed() + ' azn' ;
+document.getElementById('murik').innerHTML = aze + ' ₼ = ' + usd.toFixed() + ' dollar' ;
 document.getElementById('ss').innerHTML = aze + ' ₼ = ' + eur.toFixed() + ' euro' ;
 document.getElementById('cc').innerHTML = aze + ' ₼ = ' + tl.toFixed() + ' tl' ;
 document.getElementById('aa').innerHTML = aze + ' ₼ = ' + rus.toFixed() + ' rubl' ;
